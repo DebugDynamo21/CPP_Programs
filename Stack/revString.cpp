1714c8dbdeb1f37182bac0502e4ea464fb5915f4
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-08-11 14:32:31 +0530

    Reverse string using stack

---
+++ Stack/revString.cpp
@@ -0,0 +1,20 @@
+// Given a string, reverse it using a stack.
+
+#include <iostream>
+using namespace std;
+int main(){
+    stack<char> st;
+    string str = "Hello, World!";
+
+    for(int i : str){
+        st.push(i);
+    }
+
+    for(int i = 0; i < str.length(); i++){
+        str[i] = st.top();
+        st.pop();
+    }
+
+    cout << "Reversed string: " << str << endl;
+    return 0; 
+}
